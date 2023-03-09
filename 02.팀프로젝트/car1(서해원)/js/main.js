fa2daac2e8b0667ddbc052ea41e6077f480f1e86
shw <id201902@gmail.com>
2023-03-09 11:04:20 +0900

    123

--- 02.팀프로젝트/car1(서해원)/js/main.js
+++ 02.팀프로젝트/car1(서해원)/js/main.js
@@ -1,22 +1,21 @@
 // 로딩구역 ////////////////////
-window.addEventListener('DOMContentLoaded', loadFn);
+window.addEventListener("DOMContentLoaded", loadFn);
 
 //////////////로드함수/////////////
 function loadFn() {
-    console.log('로딩완료!');
+    console.log("로딩완료!");
 
     ///////////////////// 첫장면 글자 날라오기 이벤트//////////////////////////////////////
 
     // 1. 대상선정
     //1-A 대상 선정 : div class="title
-    const title = document.querySelector('.title');
-    
+    const title = document.querySelector(".title");
 
     // 1-B 데이터 할당
-    const mytxt = 'CAR1';
+    const mytxt = "CAR1";
 
     // 1-C. 데이터 글자 한글자씩 태그 쓰기
-    let hcode = '';
+    let hcode = "";
     let idx = 0;
 
     // for of 문 반복한다.
@@ -36,7 +35,7 @@ function loadFn() {
 
     // 5. 일정시간 후 스테이지박스에 클래스 "on"주고 애니 작동
     setTimeout(() => {
-        title.classList.add('on');
+        title.classList.add("on");
     }, 800);
 
     title.title = `마지막에 글자가 멈추면 클릭하세요`;
@@ -48,46 +47,46 @@ function loadFn() {
 
     ////////////////////////////CASTING 이벤트 주사위 돌리기 //////////////////////////////////
     // 1-1 이벤트 대상 : 메뉴 .actor a
-    const menu = document.querySelectorAll('.actor a');
+    const menu = document.querySelectorAll(".actor a");
     // console.log(menu);
 
     // 1-2 변경대상1  : 주사위  .squ
-    const squ = document.querySelector('.squ');
+    const squ = document.querySelector(".squ");
     const car1 = {
         Start: {
-            배우: 'Start',
-            그림: 'https://movie-phinf.pstatic.net/20111223_15/1324587194472ANYeI_JPEG/movie_image.jpg?type=m665_443_2',
+            배우: "Start",
+            그림: "https://movie-phinf.pstatic.net/20111223_15/1324587194472ANYeI_JPEG/movie_image.jpg?type=m665_443_2",
         },
-        'Lightning McQueen': {
-            배우: 'Owen Wilson',
-            그림: 'https://movie-phinf.pstatic.net/20111223_295/13245872115067L0YP_JPEG/movie_image.jpg?type=m665_443_2',
+        "Lightning McQueen": {
+            배우: "Owen Wilson",
+            그림: "https://movie-phinf.pstatic.net/20111223_295/13245872115067L0YP_JPEG/movie_image.jpg?type=m665_443_2",
         },
-        'Tow Mater': {
-            배우: 'Larry The Cable Guy',
-            그림: 'https://movie-phinf.pstatic.net/20111223_269/13245872163772lFmf_JPEG/movie_image.jpg?type=m665_443_2',
+        "Tow Mater": {
+            배우: "Larry The Cable Guy",
+            그림: "https://movie-phinf.pstatic.net/20111223_269/13245872163772lFmf_JPEG/movie_image.jpg?type=m665_443_2",
         },
-        'Doc Hudson': {
-            배우: 'Paul Newman',
-            그림: 'https://movie-phinf.pstatic.net/20111223_52/1324587214218kmhhV_JPEG/movie_image.jpg?type=m665_443_2',
+        "Doc Hudson": {
+            배우: "Paul Newman",
+            그림: "https://movie-phinf.pstatic.net/20111223_52/1324587214218kmhhV_JPEG/movie_image.jpg?type=m665_443_2",
         },
-        'Sally Carrera': {
-            배우: 'Bonnie Hunt',
-            그림: 'https://movie-phinf.pstatic.net/20111223_286/1324587217476XfKzB_JPEG/movie_image.jpg?type=m665_443_2',
+        "Sally Carrera": {
+            배우: "Bonnie Hunt",
+            그림: "https://movie-phinf.pstatic.net/20111223_286/1324587217476XfKzB_JPEG/movie_image.jpg?type=m665_443_2",
         },
-        'Chick Hicks': {
-            배우: 'Michael Keaton',
-            그림: 'https://movie-phinf.pstatic.net/20111223_97/13245872120488WJk9_JPEG/movie_image.jpg?type=m665_443_2',
+        "Chick Hicks": {
+            배우: "Michael Keaton",
+            그림: "https://movie-phinf.pstatic.net/20111223_97/13245872120488WJk9_JPEG/movie_image.jpg?type=m665_443_2",
         },
     };
 
     // 1-3 변경대상2  : 배우명 .cname
-    const cname = document.querySelector('.cname');
+    const cname = document.querySelector(".cname");
 
     // 1-4 변경대상3  : 배우 설명 박스  .squbx
-    const squbx = document.querySelector('.squbx');
+    const squbx = document.querySelector(".squbx");
 
     //1-5 변경대상4 : 캐릭터 이미지 넣기 carchac
-    const carchac = document.querySelector('.carchac');
+    const carchac = document.querySelector(".carchac");
 
     // 2. 메뉴에 클릭이벤트 설정하기
     //  for of문
@@ -102,10 +101,10 @@ function loadFn() {
             event.preventDefault();
             // 0. 박스 숨기기
 
-            squbx.style.opacity = '0';
+            squbx.style.opacity = "0";
 
             // 단위가 없는 숫자는 문자열이든 숫자든 상관없다.
-            squbx.style.transition = 'none';
+            squbx.style.transition = "none";
 
             // 이벤트 대상 :  메뉴 .actor a -> x
             // 1. 메뉴 텍스트 읽기
@@ -117,33 +116,32 @@ function loadFn() {
             // 무조건 회전하기 위해 세팅함
             let setval;
             switch (mtxt) {
-                case 'Start':
-                    setval = 'rotateX(0deg) rotateY(0deg)';
+                case "Start":
+                    setval = "rotateX(0deg) rotateY(0deg)";
                     break;
-                case 'Lightning McQueen':
-                    setval = 'rotateX(360deg) rotateY(270deg)';
+                case "Lightning McQueen":
+                    setval = "rotateX(360deg) rotateY(270deg)";
                     break;
-                case 'Tow Mater':
-                    setval = 'rotateX(-360deg) rotateY(540deg)';
+                case "Tow Mater":
+                    setval = "rotateX(-360deg) rotateY(540deg)";
                     break;
-                case 'Doc Hudson':
-                    setval = 'rotateX(720deg) rotateY(90deg)';
+                case "Doc Hudson":
+                    setval = "rotateX(720deg) rotateY(90deg)";
                     break;
-                case 'Sally Carrera':
-                    setval = 'rotateX(-450deg) rotateY(360deg)';
+                case "Sally Carrera":
+                    setval = "rotateX(-450deg) rotateY(360deg)";
                     break;
-                case 'Chick Hicks':
-                    setval = 'rotateX(450deg) rotateY(360deg)';
+                case "Chick Hicks":
+                    setval = "rotateX(450deg) rotateY(360deg)";
                     break;
                 default:
-                    setval = 'rotateX(0deg) rotateY(0deg)';
-            } //////swtich case//////////////
-            //console.log(mtxt + ":" + setval);
+                    setval = "rotateX(0deg) rotateY(0deg)";
+            } //////swtich case/////////////
 
             //3. 회전값 적용하기 (transform에 setval 변수값 할당하기)
             squ.style.transform = setval;
 
-            squ.style.transition = '1s cubic-bezier(0.65, 0.05, 0.36, 1)';
+            squ.style.transition = "1s cubic-bezier(0.65, 0.05, 0.36, 1)";
 
             // 4 .캐릭터, 배우 정보 셋팅하기
             // 변경대상1: .cname - 배우 이름
@@ -167,8 +165,8 @@ function loadFn() {
             // 대상:.squbx
 
             setTimeout(() => {
-                squbx.style.opacity = '1';
-                squbx.style.transition = 'opacity 0.4s cubic-bezier(0.29,-0.18, 0.57, 1)';
+                squbx.style.opacity = "1";
+                squbx.style.transition = "opacity 0.4s cubic-bezier(0.29,-0.18, 0.57, 1)";
             }, 1000); ///////////////타임아웃
         }; /////click 이벤트 함수 //////////////
     } ///  for of 문//////////////
